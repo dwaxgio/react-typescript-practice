2dee730d80648ba9568a1b399e333654773ddb9e
edward <edward_gio@hotmail.com>
2024-10-23 22:07:40 -0500

    Custom hook added, app updated

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./App.css";
+import { useEffect, useRef } from "react";
+import useCounter from "./hooks/useCounter";
+
+function App() {
+  const { count, increment, decrement, reset } = useCounter(0);
+  const countRef = useRef<HTMLParagraphElement>(null);
+
+  useEffect(() => {
+    if (countRef.current) {
+      if (count > 0) {
+        countRef.current.style.color = "green";
+      } else if (count < 0) {
+        countRef.current.style.color = "red";
+      } else {
+        countRef.current.style.color = "black";
+      }
+    }
+  }, [count]);
+
+  return (
+    <div style={{ textAlign: `center`, marginTop: `50px` }}>
+      <h1>Counter</h1>
+      <p ref={countRef}>
+        Current value: <strong>{count}</strong>
+      </p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+export default App;
